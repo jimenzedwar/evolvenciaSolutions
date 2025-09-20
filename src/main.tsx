6091c7c42b96a76a1d9e7f0fbcf0462b89dc996e
Edwar Jimenez <123136963+jimenzedwar@users.noreply.github.com>
2025-09-20 13:11:02 -0400

    feat: bootstrap cozycommerce starter

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App, { GettingStartedPage, HomePage } from './App'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'getting-started',
+        element: <GettingStartedPage />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
